698746ea8357d47f75b7408c46b13629d90ee84f
ubitch69 <69955036+ubitch69@users.noreply.github.com>
2022-07-12 14:44:43 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#              Script Auto Install              #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9/10 & Ubuntu 18/20 64 bit         #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Modified by Unknown                           #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Allah                                         #"
+echo -e "# My Family                                     #"
+echo -e "# Google                                        #"
+echo -e "# Owner VPS                                     #"
+echo -e "#                                               #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
